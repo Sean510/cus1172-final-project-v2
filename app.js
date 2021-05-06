bf832402da5eba34d7ffc655d0ddf4e07be5a2e9
Sean510 <swhite51096@gmail.com>
2021-05-05 22:24:00 -0400

    Update and rename restAPI_example.js to app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.PORT || 5000
 
-var api_routes = require('./api_routes_dev.js');
+var api_routes = require('./api_routes.js');
 app.use('/api', api_routes);
 
-// static file route
-app.use('/demo', express.static('front_end'));
 
 
 // Start server
-app.listen(3000, function() {
+app.listen(PORT, function() {
     
     console.log("Server is running")
     
